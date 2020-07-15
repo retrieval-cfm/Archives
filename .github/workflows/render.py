97ad9f52212bf22c91cabcc70d6687731b293b34
xiaosu-zhu <xiaosu.zhu@outlook.com>
2020-07-15 11:34:01 +0800

    update render.py

--- .github/workflows/render.py
+++ .github/workflows/render.py
@@ -38,13 +38,14 @@ def getAllSubmodules():
 
 allSubmodules = getAllSubmodules()
 
-def appendNodes(root: str, rootNode: Node):
+def appendNodes(root: str, rootNode: Node, level: int, maxLevel: int):
     for item in os.listdir(root):
         path = os.path.join(root, item)
         if os.path.isdir(path):
             newNode = Node(item, path)
             rootNode.dirs.append(newNode)
-            appendNodes(path, newNode)
+            if level + 1 < maxLevel:
+                appendNodes(path, newNode, level + 1, maxLevel)
         else:
             rootNode.files.append(path)
 
@@ -55,7 +56,7 @@ def determineIfSubmodule(path: str):
         return allSubmodules[path]
     return path
 
-def renderSinglePaper(fileName: str):
+def renderSinglePaper(fileName: str, codeBase: list):
     paperName = fileName.split(".")[0].split("]")[-1]
     year = int(fileName.split('[')[-1][0:2])
     base = datetime.date.today().year + 1
@@ -65,23 +66,24 @@ def renderSinglePaper(fileName: str):
     else:
         year += prefix
     result = f"* {year} - "
-    if os.path.exists(os.path.join("Codes", paperName)):
+    if os.path.join("Codes", paperName) in codeBase:
         codePath = determineIfSubmodule(os.path.join("Codes", paperName))
         result += f"[**[Code]**]({codePath}) "
+        codeBase.remove(os.path.join("Codes", paperName))
     result +=  f"[{paperName}]({urllib.parse.quote(fileName)})"
     return result, year
 
 def renderSingleFolder(node: Node):
     return f"* [{node.name}]({urllib.parse.quote(node.fullPath)})"
 
-def renderPapers(node: Node, level: int):
+def renderPapers(node: Node, level: int, codeBase: list):
     a = f"{'#' * (level + 1)} {node.name}\r\n"
-    items = [renderSinglePaper(x) for x in node.files]
+    items = [renderSinglePaper(x, codeBase) for x in node.files]
     items.sort(key=lambda x: x[1])
     a += "\r\n".join(x[0] for x in items)
     a += "\r\n"
     for i in node.dirs:
-        a += renderPapers(i, level + 1)
+        a += renderPapers(i, level + 1, codeBase)
     return a
 
 def renderTutorials(node: Node, level: int):
@@ -89,20 +91,54 @@ def renderTutorials(node: Node, level: int):
     items = [renderSingleFolder(x) for x in node.dirs]
     return a + "\r\n".join(items)
 
+def findOutAllCodeDir():
+    rootNode = Node("Codes", "Codes")
+    appendNodes("Codes", rootNode, 0, 2)
+    result = []
+    def appendResult(n: Node):
+        for f in n.files:
+            result.append(f)
+        for node in n.dirs:
+            if node.fullPath in allSubmodules:
+                result.append(node.fullPath)
+                continue
+            appendResult(node)
+        if len(n.dirs) == 0:
+            result.append(n.fullPath)
+    appendResult(rootNode)
+    return result
+
+def renderCodes(codeBase: list):
+    result = "# Codes\r\n"
+    codes = []
+    for c in codeBase:
+        fileName = os.path.basename(c)
+        if c in allSubmodules:
+            codes.append(f"* [{fileName}]({allSubmodules[c]})")
+        else:
+            codes.append(f"* [{fileName}]({c})")
+    result += "\r\n".join(codes)
+    return result
+
 if __name__ == "__main__":
     with open("README.md", "w") as fp:
         fp.write("![Auto Render README.md](https://github.com/retrieval-cfm/Archives/workflows/Auto%20Render%20README.md/badge.svg)\r\n\r\n")
         fp.write("**NOTE**: *Push* or merge from *Pull Requests* will trigger to render this TOC automatically.\r\n\r\n")
-        fp.write("* Please refer to [format guideline](GUIDE.md) to arrange files correctly.\r\n\r\n")
+        fp.write("**Please refer to [format guideline](GUIDE.md) to arrange files correctly**.\r\n\r\n")
         fp.write("# Table of Contents\r\n")
         
         rootNode = Node("Papers", "Papers")
-        appendNodes("Papers", rootNode)
-        results = renderPapers(rootNode, 0)
+        appendNodes("Papers", rootNode, 0, 10000)
+        codeBase = findOutAllCodeDir()
+        results = renderPapers(rootNode, 0, codeBase)
         fp.write(results)
 
 
         rootNode = Node("Tutorials", "Tutorials")
-        appendNodes("Tutorials", rootNode)
+        appendNodes("Tutorials", rootNode, 0, 10000)
         resultsT = renderTutorials(rootNode, 0)
         fp.write(f"\r\n{resultsT}")
+
+        fp.write("\r\n\r\n")
+        resultsC = renderCodes(codeBase)
+        fp.write(resultsC)
